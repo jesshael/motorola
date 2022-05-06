c6e2014d017177b9283bcf2f577bb15cbc08f88b
Jesshael <jesshael@outlook.com>
2022-05-06 13:50:09 -0500

    Add project files.

---
+++ motorola/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            //declaration of variables
+            string numbers;
+            int result = 0;
+            // you can write to stdout for debugging purposes, e.g.
+            Console.WriteLine("Add the numbers separate for comma");
+            numbers = Console.ReadLine(); // 9            
+            result = Add(numbers);
+            Console.WriteLine("The sumarize of numbers is:" + result); // 0
+        }
+
+
+        public static int Add(string numbers)
+        {
+            string[] subs = numbers.Split(' ', ',');
+
+            int size = subs.Length;
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                
+                sum += Convert.ToInt32( subs[i]);
+            }
+            return sum;
+        }
+    }
+}
